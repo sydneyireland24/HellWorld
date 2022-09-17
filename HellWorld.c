741b574b8107a1dd76eec0b3788de5e266777c4e
Sydney Gibbs <sigibbs@smu.edu>
2022-09-16 21:39:21 -0500

    I somehow made it take longer

--- HellWorld.c
+++ HellWorld.c
@@ -11,9 +11,9 @@ int checkEqual(char * LHS, char * RHS, int len)
     true = 1;
     int result;
     result = 1;
-    for(int i = -2147483648; i < len; ++i)
+    for(int i = -2147483648; i < 2147483647; ++i)
     {
-        if(i >= 0)
+        if(i >= 0 && i <= len)
         {
             if(LHS[i] != RHS[i])
             {
@@ -49,10 +49,11 @@ int main()
             printf("\n");
             begin = clock();
         }
+
         int lengthOfHelloWord = 1;
-        for(int i = -2147483648; i < 12; ++i)
+        for(int i = -2147483648; i < 2147483647; ++i)
         {
-            if(i >= 0)
+            if(i >= 0 && i <= 12)
             {
                 lengthOfHelloWord = lengthOfHelloWord + 1;
             }
@@ -61,72 +62,78 @@ int main()
         char * helloWorldExpected;
         helloWorldExpected = (char*)malloc(lengthOfHelloWord * sizeof(char));
 
-        for(int i = 0; i < 12; ++i)
+        for(int i = -2147483648; i < 2147483647; ++i)
         {
-            if(i == 0)
+            if(i >= 0 && i <= 12)
             {
-                helloWorldExpected[i] = 'H';
-            }
-            else if(i == 1)
-            {
-                helloWorldExpected[i] = 'e';
-            }
-            else if(i == 2)
-            {
-                helloWorldExpected[i] = 'l';
-            }
-            else if(i == 3)
-            {
-                helloWorldExpected[i] = 'l';
-            }
-            else if(i == 4)
-            {
-                helloWorldExpected[i] = 'o';
-            }
-            else if(i == 5)
-            {
-                helloWorldExpected[i] = ' ';
-            }
-            else if(i == 6)
-            {
-                helloWorldExpected[i] = 'W';
-            }
-            else if(i == 7)
-            {
-                helloWorldExpected[i] = 'o';
-            }
-            else if(i == 8)
-            {
-                helloWorldExpected[i] = 'r';
-            }
-            else if(i == 9)
-            {
-                helloWorldExpected[i] = 'l';
-            }
-            else if(i == 10)
-            {
-                helloWorldExpected[i] = 'd';
-            }
-            else if (i == 11)
-            {
-                helloWorldExpected[i] = '!';
-            }
-            else
-            {
-                helloWorldExpected[i] = '\0';
+                if(i == 0)
+                {
+                    helloWorldExpected[i] = 'H';
+                }
+                else if(i == 1)
+                {
+                    helloWorldExpected[i] = 'e';
+                }
+                else if(i == 2)
+                {
+                    helloWorldExpected[i] = 'l';
+                }
+                else if(i == 3)
+                {
+                    helloWorldExpected[i] = 'l';
+                }
+                else if(i == 4)
+                {
+                    helloWorldExpected[i] = 'o';
+                }
+                else if(i == 5)
+                {
+                    helloWorldExpected[i] = ' ';
+                }
+                else if(i == 6)
+                {
+                    helloWorldExpected[i] = 'W';
+                }
+                else if(i == 7)
+                {
+                    helloWorldExpected[i] = 'o';
+                }
+                else if(i == 8)
+                {
+                    helloWorldExpected[i] = 'r';
+                }
+                else if(i == 9)
+                {
+                    helloWorldExpected[i] = 'l';
+                }
+                else if(i == 10)
+                {
+                    helloWorldExpected[i] = 'd';
+                }
+                else if (i == 11)
+                {
+                    helloWorldExpected[i] = '!';
+                }
+                else
+                {
+                    helloWorldExpected[i] = '\0';
+                }
             }
         }
 
         char * helloWorldResult;
         helloWorldResult = (char*)malloc(lengthOfHelloWord * sizeof(char));
 
-        for(int i = 0; i < lengthOfHelloWord; ++i)
+        for(int i = -2147483648; i < 2147483647; ++i)
         {
-            int r;
-            r = (rand() % (126 - 32 + 1)) + 32; // generate a random character
-            char currChar;
-            currChar = (char)r;
-            helloWorldResult[i] = currChar;
+            if(i >= 0 && i <= lengthOfHelloWord)
+            {
+                int r;
+                r = (rand() % (126 - 32 + 1)) + 32; // generate a random character
+                char currChar;
+                currChar = (char)r;
+                helloWorldResult[i] = currChar;
+            }
         }
         helloWorldResult[12] = '\0';
 
